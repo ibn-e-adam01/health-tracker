053fa45f1c9d4562dd8bd7418f675f9dd066a804
user.name: "Asrar Firdoos <user.email: "theasrarfirdous@gmail.com>
2026-07-31 18:02:53 +0530

    feat: modify environment variables

--- backend_NodeJS/app.js
+++ backend_NodeJS/app.js
@@ -14,11 +14,6 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
 
-const allowedOrigins = [
-   process.env.VITE_FRONTEND_URL,           // It's my absolute main Vercel URL
-  'http://localhost:5173'                  // It Keeps local testing functional
-];
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
